a4b9747d2a955f7c63df4638b15e021f7cf864bb
Nalini Prasad Dash <91181094+nintendoit@users.noreply.github.com>
2024-09-23 08:59:01 +0530

    Added notification sound

---
+++ assets/README.md
@@ -0,0 +1,2 @@
+###### Notificatoin sound credit:
+Blip_C_07 by cabled_mess -- https://freesound.org/s/350860/ -- License: Creative Commons 0
--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const logRectangle = document.querySelector("#rect-log");
 const actionButton = document.querySelector("#action-btn");
 const permissionButton = document.querySelector("#notif-permission-btn");
 const permissionStatusText = document.querySelector("#permission-status-txt");
+const audio = new Audio('assets/350860__cabled_mess__blip_c_07.wav');
 
 const workStartContent = document.createElement("div");
 workStartContent.textContent = "Work session started!";
@@ -21,9 +22,11 @@ actionButton.onclick = () => {
   setTimeout(() => {
     logRectangle.appendChild(relaxStartContent);
     new Notification("Good work!", { body: "Take a 5 minute break!" });
+    audio.play();
     setTimeout(() => {
       logRectangle.appendChild(relaxEndContent);
       new Notification("Back to work!", { body: "Write down the tasks!" });
+      audio.play();
     }, 5 * 60 * 1000);
   }, 25 * 60 * 1000);
 };
